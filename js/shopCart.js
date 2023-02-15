b44c8cfd96d8ea0394a99cb80fb17cbe246c71e9
Cataraga Maxim <93079459+CataragaMaxim@users.noreply.github.com>
2023-02-15 12:16:05 +0200

    Add files via upload

--- js/shopCart.js
+++ js/shopCart.js
@@ -2,6 +2,7 @@ const plusEl = document.querySelectorAll('.plus');
 const minusEl = document.querySelectorAll('.minus');
 const products = document.querySelector('#product-page');
 const cart = document.querySelector('.shop-cart');
+const cartProducts = document.querySelector('.shop-cart .cart-prod');
 // const clearCartBtn = document.querySelector('.clear-cart');
 // const empty = document.querySelector('.empty');
 
@@ -50,21 +51,51 @@ function getProductInfo(product) {
 function addToCart(product){
   const prod = document.createElement('div');
   for (let i = 0; i < cart.childNodes.length; i++) {
-    if (cart.childNodes[i].children[1].textContent == product.name) {
-      cart.childNodes[i].children[3].textContent = parseInt(cart.childNodes[i].children[3].textContent) + parseInt(product.amount);
-      cart.childNodes[i].children[2].textContent = parseInt(product.price)*(parseInt(cart.childNodes[i].children[3].textContent));
+    if (cart.childNodes[i].children[1].children[0].textContent == product.name) {
+      cart.childNodes[i].children[2].children[1].textContent = parseInt(cart.childNodes[i].children[3].textContent) + parseInt(product.amount);
+      cart.childNodes[i].children[1].children[2].textContent = parseInt(product.price)*parseInt(cart.childNodes[i].children[2].children[1].textContent);
       return;
     }
   }
   prod.classList.add('cart-prod','cart-toggle')
   prod.innerHTML = `
   <img src="${product.image}" class="prod-img">    
-  <h5>${product.name}</h5>
-  <p>${parseInt(product.price)*parseInt(product.amount)}</p>
-  <p>${product.amount}</p>
+  <div class="prod-info">
+    <h5>${product.name}</h5>
+    <p alt="${product.price}"><p>${parseInt(product.price)*parseInt(product.amount)}</p> lei</p>
+  </div>
+  <div>  
+    <button class="minus">-</button>
+    <p class="amount">1</p>
+    <button class="plus">+</button>
+  </div>
   <img src="img/remove.png" class="remove">
 `;
+  for (let i = 0; i < cartProducts.length; i++) {
+
+  };
   cart.appendChild(prod);
+}
+
+function plusCart(e) {
+  let amount = parseInt(e.target.previousElementSibling.innerHTML);
+  let price = parseInt(e.target.parentElement.parentElement.children[1].children[1].alt);
+  amount = amount + 1;
+  console.dir(e.target);
+  e.target.previousElementSibling.innerHTML = amount;
+
+}
+
+function minusCart(e) {
+  let amount = parseInt(e.target.nextElementSibling.innerHTML);
+  if (amount <= 1) {
+    amount = 1;
+    e.target.nextElementSibling.innerHTML = amount;
+    return e.target.parentElement.parentElement.children[1].children[2].textContent = parseInt(price)*parseInt(amount);
+  }
+  amount = amount - 1;
+  e.target.nextElementSibling.innerHTML = amount;
+  return e.target.parentElement.parentElement.children[1].children[2].textContent = parseInt(price)*parseInt(amount);
 }
 
 function removeProduct(e){
@@ -74,9 +105,9 @@ function removeProduct(e){
 }
 
 function clearCart(){
-  console.log(cart.firstChild)
-  while(cart.firstChild){
-      cart.removeChild(cart.firstChild);
+  console.log(cartProducts.firstChild)
+  while(cartProducts.firstChild){
+      cart.removeChild(cartProducts.firstChild);
   }
 }
 
